cbdf53b6675475db82e8035d05ed9909b95697b7
Maximilian <maumaa19@htl-kaindorf.at>
2023-03-31 12:08:50 +0200

    First Websocket Logic

---
+++ chess/src/main/java/at/kaindorf/chess/websocket/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package at.kaindorf.chess.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+    @Configuration
+    @EnableWebSocketMessageBroker
+    public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+        @Override
+        public void configureMessageBroker(MessageBrokerRegistry config) {
+            config.enableSimpleBroker("/sub"); //Subscription-Endpoint
+            config.setApplicationDestinationPrefixes("/send"); //Event-Router für CLIENT -> Server
+        }
+
+        @Override
+        public void registerStompEndpoints(StompEndpointRegistry registry) {
+            registry.addEndpoint("/game").withSockJS();
+        }
+
+    }
