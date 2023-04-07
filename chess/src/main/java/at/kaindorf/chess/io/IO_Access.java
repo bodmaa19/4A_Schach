1e932bfca796a86b3a286d81ccbe6cc0d766469f
Your Name <you@example.com>
2023-04-07 19:55:09 +0200

    07.04.2023 neuer Pfad

--- chess/src/main/java/at/kaindorf/chess/io/IO_Access.java
+++ chess/src/main/java/at/kaindorf/chess/io/IO_Access.java
@@ -14,7 +14,7 @@ public class IO_Access
 {
     public static List<User> readUserFromJson() throws IOException
     {
-        Path path = Path.of("D:\\POS4\\PROJECT\\HELP\\schach_sb\\src\\main\\resources\\user.json");
+        Path path = Path.of(System.getProperty("user.dir"), "src", "main", "resources", "user.json");
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper.readValue(path.toFile(), new TypeReference<List<User>>(){});
@@ -22,7 +22,7 @@ public class IO_Access
 
     public static void writeUserToJson(List<User> userList) throws IOException, URISyntaxException
     {
-        Path path = Path.of("D:\\POS4\\PROJECT\\HELP\\schach_sb\\src\\main\\resources\\user.json");
+        Path path = Path.of(System.getProperty("user.dir"), "src", "main", "resources", "user.json");
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.writerWithDefaultPrettyPrinter().writeValue(path.toFile(), userList);
