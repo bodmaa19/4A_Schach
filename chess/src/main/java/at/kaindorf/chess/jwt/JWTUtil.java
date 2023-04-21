306f2d4b31fbf0231a08d746830346c5df375344
Roman <lanroa19@htl-kaindorf.at>
2023-04-21 08:36:12 +0200

    test

--- chess/src/main/java/at/kaindorf/chess/jwt/JWTUtil.java
+++ chess/src/main/java/at/kaindorf/chess/jwt/JWTUtil.java
@@ -6,27 +6,21 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
-public class JWTUtil
-{
+public class JWTUtil {
     private static final String JWT_SECRET = "This-is-my-secret-to-encrypt-all-the-data-!";
 
-    public static String generateToken(String username)
-    {
+    public static String generateToken(String username) {
         Algorithm algorithm = Algorithm.HMAC256(JWT_SECRET);
         return JWT.create().withIssuer("auth0").withSubject(username).sign(algorithm);
     }
 
-    public static boolean verifyToken(String token)
-    {
-        try
-        {
+    public static boolean verifyToken(String token) {
+        try {
             Algorithm algorithm = Algorithm.HMAC256(JWT_SECRET);
             JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build();
             DecodedJWT jwt = verifier.verify(token);
             return true;
-        }
-        catch (JWTVerificationException exception)
-        {
+        } catch (JWTVerificationException exception) {
             return false;
         }
     }
